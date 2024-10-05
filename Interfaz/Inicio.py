32cd2215d5e896338317f4d53440ffdbe6a1fba2
XxKoosLoos <L211080382@iztapalapa.tecnm.mx>
2024-10-05 03:10:53 -0600

    Actualizacion codigo

--- Interfaz/Inicio.py
+++ Interfaz/Inicio.py
@@ -1,20 +1,27 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, \
-    QLabel
 import sys
-
-
-class Ventana(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        print('Creando la ventana')
+from PyQt6.QtWidgets import QApplication
+from main_classes import SplashScreen, MainWindow
 
 def main():
-    print('Dentro de main')
     app = QApplication(sys.argv)
-    ventana = Ventana()
-    ventana.show()
-    sys.exit(app.exec())
 
+    # Mostrar la pantalla de carga
+    splash = SplashScreen()
+    splash.show()
+
+    # Crear la ventana principal pero no la mostramos aún
+    main_window = MainWindow()
+
+    def on_splash_finished():
+        # Mostrar la ventana principal y cerrar la pantalla de carga
+        main_window.show()
+        splash.close()
+
+    # Conectar la finalización de la pantalla de carga con el inicio de la ventana principal
+    splash.finished.connect(on_splash_finished)
+
+    # Ejecutar la aplicación
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
